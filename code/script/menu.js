9311db6f0092b991e8691ea56508354ef3676d1b
csete-andor <admin@cseteandor.hu>
2024-04-18 17:05:36 +0200

    ipdate

---
+++ code/script/menu.js
@@ -0,0 +1,17 @@
+let IsMenuOpen = false;
+let menu = document.getElementById("menu");
+let menuItems = document.getElementById("menuItems");
+document.getElementById("menu").addEventListener("click", menuClicked);
+function menuClicked() {
+  if (IsMenuOpen == true) {
+    IsMenuOpen = false;
+    menu.style.backgroundImage = "url(images/menu.open.svg)";
+    menuItems.classList.remove("open")
+    document.body.style.overflow = "scroll";
+  } else {
+    IsMenuOpen = true;
+    menuItems.classList.add("open")
+    menu.style.backgroundImage = "url(images/menu.closed.svg)";
+    document.body.style.overflow = "hidden";
+  }
+}
