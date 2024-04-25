87e713925aecbcbc9c37ee33eaf1626cfb5c6a96
UltraHal <admin@cseteandor.hu>
2024-04-25 19:10:37 +0200

    final?

--- code/script/menu.js
+++ code/script/menu.js
@@ -5,13 +5,13 @@ document.getElementById("menu").addEventListener("click", menuClicked);
 function menuClicked() {
   if (IsMenuOpen == true) {
     IsMenuOpen = false;
-    menu.style.backgroundImage = "url(/code/images/menu.open.svg)";
+    menu.style.backgroundImage = "url(/Valinor/code/images/menu.open.svg)";
     menuItems.classList.remove("open")
     document.body.style.overflow = "scroll";
   } else {
     IsMenuOpen = true;
     menuItems.classList.add("open")
-    menu.style.backgroundImage = "url(/code/images/menu.closed.svg)";
+    menu.style.backgroundImage = "url(/Valinor/code/images/menu.closed.svg)";
     document.body.style.overflow = "hidden";
   }
 }
